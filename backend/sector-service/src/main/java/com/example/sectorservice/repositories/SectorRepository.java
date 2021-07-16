e0ab3d533bc60fbed98f6d2c3c96f38155c828fa
udisinghania <udisinghania1998@gmail.com>
2021-07-16 15:14:35 +0530

    added-sector-service

---
+++ backend/sector-service/src/main/java/com/example/sectorservice/repositories/SectorRepository.java
@@ -0,0 +1,8 @@
+package com.example.sectorservice.repositories;
+
+import com.example.sectorservice.entities.Sector;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SectorRepository extends CrudRepository<Sector, String> {
+
+}
